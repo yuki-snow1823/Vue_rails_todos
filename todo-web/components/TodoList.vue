f911acac1cc8b35e6a1ed2874bba00bb30c34ebd
yuki-snow1823 <magic.iroha@gmail.com>
2020-03-25 10:27:35 +0900

    [start]postエラー起こすまで

---
+++ todo-web/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Todo List
+      <v-spacer></v-spacer>
+      <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
+    </v-card-title>
+    <v-data-table :headers="headers" :items="todos" :search="search"></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["todos"], // <- これ！
+  data() {
+    return {
+      search: "",
+      headers: [
+        {
+          text: "タイトル",
+          align: "left",
+          sortable: false,
+          value: "title"
+        },
+        { text: "ユーザー名", value: "username" }
+       // ここのtodosはpropsで受け取るので削除します
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ todo-web/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <AddTodo @submit="addTodo" />
+    <TodoList :todos="todos" />
+  </div>
+</template>
+
+<script>
+import AddTodo from "@/components/AddTodo";
+import TodoList from "@/components/TodoList";
+
+export default {
+  components: {
+    AddTodo,
+    TodoList
+  },
+  created() {
+     console.log("API_KEY:", process.env.API_KEY);  //<= これ追加
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        title
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
